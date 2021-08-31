5cf0e7645d353a2b77cb77999653d8d1bca71d98
yuxuan.dong <yuxuan.dong@ucloud.cn>
2021-08-31 11:18:14 +0800

    moditfy hammerspoon script

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-
 local hyperCA = {"ctrl", "alt"}
+local hyperAC = {"alt", "cmd"}
 local alt = {"alt"}
 local cmd = {"cmd"}
 
@@ -15,9 +15,10 @@ local appShortcutMapping = {
     t = "iTerm",
     g = "Google Chrome",
     e = "企业微信",
-    w = "微信",
+    w = "WeChat",
     r = "QQ",
-    v = "visual Studio Code"
+    v = "Visual Studio Code",
+    n = "neteasemusic",
 }
 for key, app in pairs(appShortcutMapping) do
     hs.hotkey.bind(alt, key, function()
@@ -32,40 +33,48 @@ hs.hotkey.bind(cmd, '`', function()
     local rect = nextScreen:fullFrame()
     local center = hs.geometry.rectMidPoint(rect)
 
-    hs.mouse.setAbsolutePosition(center)
+    hs.mouse.absolutePosition(center)
+    hs.eventtap.leftClick(center)
 end)
-movetoscreen = function(win, n) 
-    local screens = hs.screen.allScreens()
-    if n > #screens then
-        hs.alert.show("Only " .. #screens .. " monitors ")
-    else
-        local toWin = screens[n]:name()
-        hs.alert.show("Move " .. win:application():name() .. " to " .. toWin)
-        hs.layout.apply({{nil, win:title(), toWin, hs.layout.maximized, nil, nil}})
-    end
-end
+
 -- move cursor to monitor 1/2/3/4 and maximize the window
 for n= 1, 4 do
-    hs.hotkey.bind(hyperCA, tostring(n), function() movetoscreen(hs.window.focusedWindow(), n) end)
+    hs.hotkey.bind(hyperCA, tostring(n), function()
+	local win = hs.window.focusedWindow()
+	local app = win:application()
+	local screens = hs.screen.allScreens()
+	if n > #screens then 
+	    hs.alert.show("Only " .. #screens .. " monitors")
+	else
+	    local toScreen = screens[n]
+	    hs.alert.show("Move " .. app:name() .." to " .. toScreen:name())
+	    hs.layout.apply({{app, nil, toScreen, hs.layout.maximized, nil, nil}})
+	end
+    end)
 end
-
 -- switch active window
-hs.hotkey.bind(hyperCA, '/', function() hs.hints.windowHints() end)
+-- hs.hotkey.bind(hyperCA, '/', function() hs.hints.windowHints() end)
+-- switcher_vscode = hs.window.switcher.new{"Visual Studio Code"}
+switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
+
+hs.hotkey.bind(alt, 'tab', 'Next window',function()switcher:next()end)
 
 -- translator
 
+
+
 -- reload
 -- http://www.hammerspoon.org/go/#fancyreload
-function reloadConfig(files)
-	doReload = false
-	for _, file in pairs(files) do
-		if file:sub(-4) == ".lua" then
-			doReload = true
-		end
-	end
-	if doReload then
-		hs.reload()
-	end
-end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon", reloadConfig):start()
-hs.alert.show("Hammerspoon Config Reloaded")
+-- function reloadConfig(files)
+-- 	doReload = false
+-- 	for _, file in pairs(files) do
+-- 		if file:sub(-4) == ".lua" then
+-- 			doReload = true
+-- 		end
+-- 	end
+-- 	if doReload then
+-- 		hs.reload()
+-- 	end
+-- end
+-- hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon", reloadConfig):start()
+-- hs.alert.show("Hammerspoon Config Reloaded")
